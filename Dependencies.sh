be357623822dd81060d69e9274ca8ac22268ec4a
Yudel <yam3nv@virginia.edu>
2020-01-30 22:06:52 -0500

    Updated Dependencies for raspbian lite

--- Dependencies.sh
+++ Dependencies.sh
@@ -1,1 +1,5 @@
-sudo pip3 install adafruit-circuitpython-tsl2591 && sudo pip3 install adafruit-circuitpython-bme280 && pip3 install adafruit-io
+sudo pip3 install --upgrade setuptools
+sudo pip3 install RPI.GPIO
+sudo pip3 install adafruit-circuitpython-tsl2591
+sudo pip3 install adafruit-circuitpython-bme280
+sudo pip3 install adafruit-io
