b31d046e36389d35cdd820f1e763bb08cc252295
Yudel <yam3nv@virginia.edu>
2020-05-11 22:39:33 -0400

    Sound.py needs fix - tabs/spaces

--- Beacon.sh
+++ Beacon.sh
@@ -2,4 +2,4 @@ sudo hciconfig hci0 down
 sudo hciconfig hci0 up
 sudo hciconfig hci0 leadv 3
 sudo hciconfig hci0 noscan
-sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 E2 0A 39 F4 73 F5 4B C4 A1 2F 17 D1 AD 07 A9 61 00 00 00 00 C8 00
+sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 B9 40 7F 30 F5 F8 46 6E AF F9 25 55 6B 57 FE 6D 41 01 BE 51 C8 00
--- Sound.py
+++ Sound.py
@@ -1,27 +1,80 @@
 import pyaudio
 import numpy as np
 
-CHUNKSIZE = 1024 # fixed chunk size
-RATE = 44100
+CHUNKSIZE = 64 # fixed chunk size
+RATE = 8000
 
 # initialize portaudio
 p = pyaudio.PyAudio()
 
-# Get input device number
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
-stream = p.open(format=pyaudio.paInt16, channels=2, rate=RATE,
+# -- This code snippet was used to determine ID of I2S Microphone -- #
+
+# Get input device number
+#info = p.get_host_api_info_by_index(0)
+#numdevices = info.get('deviceCount')
+#for i in range(0, numdevices):
+#        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+#            print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+
+# -- Init *MUST* be called before any of the other functions -- #
+#it must also be called if Terminate() is called to resume audio stream
+def Init():
+    stream = p.open(format=pyaudio.paInt32, channels=1, rate=RATE,
                 input=True, frames_per_buffer=CHUNKSIZE,
                 input_device_index = 2)
 
-# do this as long as you want fresh samples
-data = stream.read(CHUNKSIZE)
-numpydata = np.fromstring(data, dtype=np.int16)
+first_half = []
+second_half = []
+combinedData = []
+counter = 0
 
-# close stream
-stream.stop_stream()
-stream.close()
+def AnalyzeData():
+    #Use the Python audio lib that Nutta Sent to Analyze the Audio File
+    #Use System Call to Start pyAudioAnalysis -> ShortTermFeatures.py with file name
+
+    return
+
+def WriteData():
+    global combinedData
+    combinedData = []
+    if counter%2 == 0:
+        combinedData.append(second_half)
+        combinedData.append(first_half)
+    else:
+        combinedData.append(first_half)
+        combinedData.append(second_half)
+
+    #Write to .wav file on another thread
+
+
+
+# -- Gathers data from the micrphone and calls WriteData then AnalyzeData() -- #
+def GetData():
+    global first_half, second_half, counter
+    npd = []
+    #d = []
+
+    # -- Take 250ms of sound and put it in either first/second half -- #
+    for i in range(0, int(RATE/CHUNKSIZE * .25)):
+        data = stream.read(CHUNKSIZE)
+        numpydata = np.frombuffer(data, dtype=np.uint32)
+	#d.append(data)
+	npd.append(numpydata)
+    if counter % 2 == 0:
+        first_half = numpydata
+    else:
+        second_half = numpydata
+
+    WriteData()
+    AnalyzeData()
+    counter += 1
+
+def StopAudoStream():
+    stream.stop_stream()
+    stream.close()
+
+def Terimante():
+    stream.stop_stream()
+    stream.close()
