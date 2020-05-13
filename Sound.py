10dff83057712cae007e54e7963bda7a0716ada1
Yudel <yam3nv@virginia.edu>
2020-05-13 00:10:19 -0400

    Have to debug things in -not- a terminal window

--- Sound.py
+++ Sound.py
@@ -1,10 +1,15 @@
 import pyaudio
 import numpy as np
+import wave
+import thread
+import time
+from pyAudioAnalysis import ShortTermFeatures
 
-CHUNKSIZE = 64 # fixed chunk size
-RATE = 8000
+CHUNKSIZE = 64   # fixed chunk size
+RATE = 8000      # 8kHz Sampling rate
+SAMPLE_DUR = 0.5 # 500ms sampling with 250ms overlap
 
-# initialize portaudio
+# initialize pyaudio
 p = pyaudio.PyAudio()
 
 
@@ -21,6 +26,7 @@ p = pyaudio.PyAudio()
 # -- Init *MUST* be called before any of the other functions -- #
 #it must also be called if Terminate() is called to resume audio stream
 def Init():
+    global p
     stream = p.open(format=pyaudio.paInt32, channels=1, rate=RATE,
                 input=True, frames_per_buffer=CHUNKSIZE,
                 input_device_index = 2)
@@ -30,13 +36,25 @@ second_half = []
 combinedData = []
 counter = 0
 
-def AnalyzeData():
-    #Use the Python audio lib that Nutta Sent to Analyze the Audio File
-    #Use System Call to Start pyAudioAnalysis -> ShortTermFeatures.py with file name
-
+def AnalyzeData(data):
+    F, f_names = ShortTermFeatures.feature_extraction(data, RATE, SAMPLE_DUR*RATE, (SAMPLE_DUR/2)*RATE)
+    # F is the data, so save that to the CSV file for audio data
     return
 
-def WriteData():
+
+#Only to be used in case that you want to store audio samples 
+def WriteData(data):
+    t = Time.time() # use to generate timestamp for file name
+    filename = "RawAudio.wav" # <- file naming convention
+    wf = wave.open(filename,'wb') #Open wave file in write bytes mode
+    wf.setchannels(1)     #Mono channel audio
+    wf.setsampwidth(4)    #Using 32-bit audio samples - 4 bytes
+    wf.setframerate(RATE) #Set the sampling freq of audio
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    return 
+
+def CombineData(first_half,second_half):
     global combinedData
     combinedData = []
     if counter%2 == 0:
@@ -46,9 +64,19 @@ def WriteData():
         combinedData.append(first_half)
         combinedData.append(second_half)
 
+    AnalyzeData(combinedData)
+
+    # -- NO LONGER NEEDED -- #
     #Write to .wav file on another thread
+    #WriteData(combinedData)
 
-
+def DataThread(fh,sh):
+    #Copy data
+    f_h = fh[:]
+    s_h = sh[:]
+    #Do everything in a new thread
+    CombineData(f_h,s_h)
+    return
 
 # -- Gathers data from the micrphone and calls WriteData then AnalyzeData() -- #
 def GetData():
@@ -61,14 +89,13 @@ def GetData():
         data = stream.read(CHUNKSIZE)
         numpydata = np.frombuffer(data, dtype=np.uint32)
 	#d.append(data)
-	npd.append(numpydata)
+	#npd.append(numpydata)
     if counter % 2 == 0:
-        first_half = numpydata
+        first_half = npd[:]
     else:
-        second_half = numpydata
+        second_half = npd[:]
 
-    WriteData()
-    AnalyzeData()
+    DataThread(first_half,second_half)
     counter += 1
 
 def StopAudoStream():
