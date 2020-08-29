36ae6d6469f78100af62ea631b43b61d2c2b1c93
yam3nv <yam3nv@virginia.edu>
2020-08-29 11:28:14 -0400

    Comments

--- BME.py
+++ BME.py
@@ -4,12 +4,18 @@ import busio
 import time
 import adafruit_bme280
 
-# OR create library object using our Bus SPI port
+# create SPI interface object
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 bme_cs = digitalio.DigitalInOut(board.D5)
 bme280 = None
 
+# Initialize bme280 
 def init():
+	"""Initialization function for BME280
+	   Attempts to connect to the Temperature Pressure Humidity Sensor over SPI protocol.
+	Returns:
+		int: 0 means OK -1 means there was an error
+	"""
 	global bme280
 	try:
 	    bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, bme_cs)
@@ -19,6 +25,11 @@ def init():
             return -1
 
 def temp():
+	"""Gather temperature reading from BME280
+
+	Returns:
+		int: Temperature reading or -1 for error
+	"""
 	try:
 	    return bme280.temperature
 	except:
@@ -26,6 +37,11 @@ def temp():
             return -1
 
 def humidity():
+	"""Gather humidity reading from BME280
+
+	Returns:
+		int: Humidity reading or -1 for error
+	"""	
 	try:
 	    return bme280.humidity
 	except:
@@ -33,6 +49,11 @@ def humidity():
             return -1
 
 def pressure():
+	"""Gather pressure reading from BME280
+
+	Returns:
+		int: Pressure reading or -1 for error
+	"""
 	try:
 	    return bme280.pressure
 	except:
@@ -41,6 +62,11 @@ def pressure():
 
 
 def data():
+	"""Gather readings from all three BME280 Sensors
+
+	Returns:
+		int array: [temp,humidity,pressure] ; -1 for any error values
+	"""
 	try:
 	    return [temp(),humidity(),pressure()]
 	except:
