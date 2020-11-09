a36f7385b603d2ad264af08246ef00cf62f421e7
Yudel Martinez <yam3nv@virginia.edu>
2020-11-09 14:11:38 -0500

    Requirements and dependencies

--- scripts/Dependencies.sh
+++ scripts/Dependencies.sh
@@ -5,8 +5,5 @@
 yes | sudo apt-get install python3-pip 
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade setuptools
-sudo pip3 install RPI.GPIO
-sudo pip3 install adafruit-circuitpython-tsl2591
-sudo pip3 install adafruit-circuitpython-bme280
-sudo pip3 install adafruit-io
-sudo pip3 install -r packages.pk  
+yes | sudo apt-get install libjack-jackd2-dev portaudio19-dev
+sudo pip3 install -r requirements.txt
