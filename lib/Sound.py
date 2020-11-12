bcbdf82bf0bf88aa63f3e15cdfbba4628d26cfd5
yam3nv <yam3nv@virginia.edu>
2020-11-11 19:19:21 -0500

    Added identify devices

--- lib/Sound.py
+++ lib/Sound.py
@@ -22,12 +22,13 @@ p = pyaudio.PyAudio()
 
 # -- This code snippet was used to determine ID of I2S Microphone -- #
 
-# Get input device number
-#info = p.get_host_api_info_by_ind:ex(0)
-#numdevices = info.get('deviceCount')
-# for i in range(0, numdevices):
-#        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-#            print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+# Get input device ID numbers
+def Identify_Devices():
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+       if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+           print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 
 # -- Init *MUST* be called before any of the other functions -- #
