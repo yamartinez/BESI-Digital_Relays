61f712c309021fb76e29091bb3f453f163829e8d
yam3nv <yam3nv@virginia.edu>
2020-11-11 19:20:35 -0500

    Fixed indentation

--- lib/Sound.py
+++ lib/Sound.py
@@ -24,11 +24,11 @@ p = pyaudio.PyAudio()
 
 # Get input device ID numbers
 def Identify_Devices():
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-for i in range(0, numdevices):
-       if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-           print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 
 # -- Init *MUST* be called before any of the other functions -- #
