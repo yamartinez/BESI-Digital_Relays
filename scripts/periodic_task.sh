bfa9e803a55ac5981e0a4f09e7c42fcb598c9171
yam3nv <yam3nv@virginia.edu>
2021-02-26 18:22:52 -0500

    Testing git pull and reboot

--- scripts/periodic_task.sh
+++ scripts/periodic_task.sh
@@ -8,3 +8,9 @@ NAME="$(/usr/bin/uuid)"
 
 /usr/bin/zip $NAME.zip *.csv
 /usr/bin/python3 /home/pi/s3-uploader/upload.py $PATH $NAME.zip
+
+
+# Testing git pull and reboot - will move this to a daily thing at like 2am
+cd ..
+git pull 
+reboot
